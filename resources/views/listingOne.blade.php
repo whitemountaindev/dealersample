3d33545dc24271bf5f920d07f7227733e9911a65
Jack Bergemann <jackbergemann.cs@gmail.com>
2020-12-16 17:19:31 -0700

    test

--- resources/views/listingOne.blade.php
+++ resources/views/listingOne.blade.php
@@ -28,30 +28,6 @@
 </section>
 
 <!--=================================
- inner-intro -->
- 
- <section class="inner-intro bg-1 bg-overlay-black-70">
-  <div class="container">
-     <div class="row text-center intro-title">
-           <div class="col-md-6 text-md-left d-inline-block">
-             <h1 class="text-white">product listing </h1>
-           </div>
-           <div class="col-md-6 text-md-right float-right">
-             <ul class="page-breadcrumb">
-                <li><a href="#"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-double-right"></i></li>
-                <li><a href="#">pages</a> <i class="fa fa-angle-double-right"></i></li>
-                <li><span>listing 02</span> </li>
-             </ul>
-           </div>
-     </div>
-  </div>
-</section>
-
-<!--=================================
- inner-intro -->
-
-
-<!--=================================
 product-listing  -->
 
 <section class="product-listing page-section-ptb">
